eb83d50c10423d3aadf00822db51357aa7bcd2f9
Vicki Yang <vickiwyang@gmail.com>
2021-04-22 21:42:35 -0500

    retrieved DOIs now link directly to articles

--- client/components/Article.jsx
+++ client/components/Article.jsx
@@ -7,7 +7,7 @@ const Article = ({ info }) => {
   return (
     <article className="artCard">
       <ul className="artDetailsList">
-        <li className="artHeader">DOI: {doi}</li>
+        <li className="artHeader">DOI: <a href={`https://doi.org/${doi}`} target="_blank">{doi}</a></li>
         <li className={`${citation_count > 10000 ? "over10000Cites" : citation_count > 1000 ? "over1000Cites" : citation_count > 500 ? "over500Cites" : "under500Cites"}`}>Citation Count: {citation_count}</li>
         <li className={`${commons > 1 ? "someCommons" : "noCommons"}`}>Common: {commons}</li>
       </ul>
